a185d74da1e46460ed4583f504a742b3ba485291
Tristan Isham <tristan@isham.co>
2021-08-28 00:22:33 -0400

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'ham/paper-airplane',
+  ),
+  'versions' => 
+  array (
+    'ham/paper-airplane' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
